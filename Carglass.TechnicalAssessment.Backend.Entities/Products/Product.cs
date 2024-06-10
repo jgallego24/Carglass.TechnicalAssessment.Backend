1e3ca6a095851e875e62ec806e4bd98c444da867
jgallego24 <juan.gallego2403@gmail.com>
2024-06-10 02:23:16 +0200

    Adjusting types

--- Carglass.TechnicalAssessment.Backend.Entities/Products/Product.cs
+++ Carglass.TechnicalAssessment.Backend.Entities/Products/Product.cs
@@ -4,7 +4,7 @@ public class Product
 {
     public int Id { get; set; }
     public string ProductName { get; set; }
-    public string ProductType { get; set; }
-    public string NumTerminal { get; set; }
+    public int ProductType { get; set; }
+    public int NumTerminal { get; set; }
     public string SoldAt { get; set; }
 }
